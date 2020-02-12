b2dd0a16aee9b9cd31627f5e083d505d6ce3353f
Prafful Daga <prraful@gmail.com>
2020-02-12 11:34:41 +0530

    todo app 2

--- todo-app/app/todosview.js
+++ todo-app/app/todosview.js
@@ -6,9 +6,50 @@ var TodoItemsView = Backbone.View.extend({
         if(!options && options.model){
             throw new Error("collection model is not present!")
         }
+        this.model.on("add", this.onAddTodoItem, this)
+    },
+    events:{
+        "click #clickadd":"onClickAdd",
+        "keypress #newtodoitem":"onKeyPress"
+    },
+    onKeyPress: function(eventObject){
+        console.log(eventObject)
+        console.log(eventObject.keyCode)
+        //check for enter - 13
+        if(eventObject.keyCode == 13){
+            this.onClickAdd()
+        }
+    },
+    onAddTodoItem: function(item){
+        console.log("New todo item added!");
+        console.log(item);
+        var addView = new TodoItemView({model: item})
+        this.$el.append(addView.render().$el)
+
+    },
+    onClickAdd: function(){
+        console.log("Add button clicked!");
+        console.log(this.$("#newtodoitem").val());
+        var newitem = this.$("#newtodoitem").val()
+        //var newItemValue = 
+        if(newitem != ""){
+            var todoItem = new TodoItem({description:newitem})
+            // this.model represents the collection!
+            //adding new item will fire add event
+            //add event can be captured in intialize
+            this.model.add(todoItem)
+            //console.log(this.model);
+            this.$("#newtodoitem").val("")
+        }
+        
     },
     render: function(){
         var self = this
+        //inputfield
+        this.$el.append("<input type='text' id='newtodoitem' autofocus></input>")
+        this.$el.append("<button id='clickadd'>Add</button>")    
+
+
         this.model.each(function(todoItem){
             var view = new TodoItemView({model: todoItem})
             self.$el.append(view.render().$el)
